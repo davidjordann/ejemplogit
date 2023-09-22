97ac8d5785d331a4c480442cefc6948afd44e2f1
david jordan <juan.jordan01@uceva.edu.co>
2023-09-22 01:38:45 -0500

    se modifica

---
+++ src/main/java/com/mycompany/ejemplogit/visualizador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejemplogit;
+
+/**
+ * se agrego en comentario de la clase cual es su descripcion 
+ * @author juanj
+ */
+public class visualizador {
+    
+}
